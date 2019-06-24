9dac6f864ba59d446063c6778c9be17cae38cb62
Duncan <38371914+duncannah@users.noreply.github.com>
2019-06-24 21:24:26 +0200

    clean unused code

--- commands/lookup.js
+++ commands/lookup.js
@@ -1,7 +1,4 @@
 const { requestJSON, requestHTML } = require("../util/request");
-//const fetch = require("node-fetch");
-
-//const Discord = require("discord.js");
 
 const SITES = {
 	gelbooru: async (blacklist, tags) => {
@@ -85,68 +82,5 @@ module.exports = {
 		if (!result) return msg.reply(`couldn't find anything sweetie :(`);
 
 		msg.reply(`Score: **${result[0]}**; Rating: **${result[1]}**\n${result[2]}`);
-
-		/*
-
-		let url = `https://gelbooru.com/index.php?page=post&s=list&tags=${encodeURIComponent(txt || "")}`;
-
-		const $ = await requestHTML(url);
-
-		const posts = $(".thumbnail-preview a").filter(
-			(_, el) =>
-				!BLACKLIST.filter(
-					(t) =>
-						$("img", el)
-							.attr("title")
-							.split(" ")
-							.indexOf(t) !== -1
-				).length
-		);
-
-		if (posts.length <= 0) return msg.reply(`couldn't find anything sweetie :(`);
-
-		const postURL = "https:" + posts.eq(Math.round(Math.random() * (posts.length - 1))).attr("href");
-
-		const $p = await requestHTML(postURL);
-
-		msg.reply(
-			`Score: **${$p('[id^="psc"]').text()}**; Rating: **${$p('[id^="psc"]')
-				.parent()
-				.prev()
-				.text()
-				.substr(8)}**\n${$p("#image, source").attr("src")}`
-		);
-
-		*/
-
-		/*
-	
-		let fileURL;
-	
-		if ($p("source").length)
-			fetch($p("source").attr("src"), { method: "HEAD" }).then((res) => {
-				if (parseInt(res.headers.get("Content-Length"), 10) > 8388119) {
-					msg.reply(
-						`Score: **${$p('[id^="psc"]').text()}**; Rating: **${$p('[id^="psc"]')
-							.parent()
-							.prev()
-							.text()
-							.substr(8)}**\n${$p("source").attr("src")}`
-					);
-					fileURL = false;
-				} else fileURL = $p("source").attr("src");
-			});
-		else fileURL = $p("#image").attr("src");
-	
-		if (fileURL)
-			msg.reply(
-				`Score: **${$p('[id^="psc"]').text()}**; Rating: **${$p('[id^="psc"]')
-					.parent()
-					.prev()
-					.text()
-					.substr(8)}**`,
-				new Discord.MessageAttachment(fileURL)
-			);
-		*/
 	}
 };
