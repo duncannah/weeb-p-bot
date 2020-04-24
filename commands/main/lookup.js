8f08dbc209b8677298d56e5ea37ab8ce031c165f
Duncan <38371914+duncannah@users.noreply.github.com>
2020-04-24 04:47:57 +0200

    Better lookup result using embeds

--- commands/main/lookup.js
+++ commands/main/lookup.js
@@ -1,60 +1,84 @@
+const Discord = require("discord.js");
 const Commando = require("discord.js-commando");
 
 const { requestJSON, requestHTML } = require("../../util/request");
 const { whichGuild } = require("../../util/constants");
 
 const SITES = {
-	gelbooru: async (blacklist, tags) => {
-		let url = `https://gelbooru.com/index.php?page=post&s=list&tags=sort:random%20${encodeURIComponent(
-			tags || ""
-		).trim()}`;
+	gelbooru: {
+		name: "Gelbooru",
+		icon: "https://gelbooru.com/favicon.png",
+		look: async (blacklist, tags) => {
+			let url = `https://gelbooru.com/index.php?page=post&s=list&tags=sort:random%20${encodeURIComponent(
+				tags || ""
+			).trim()}`;
 
-		const $ = await requestHTML(url);
+			const $ = await requestHTML(url);
 
-		const posts = $(".thumbnail-preview a").filter(
-			(_, el) =>
-				(parseInt(
-					($("img", el)
-						.attr("title")
-						.match(/score:(\d)+ /) || [])[1],
-					10
-				) || -1) >= 0 &&
-				!blacklist.filter((t) => $("img", el).attr("title").split(" ").indexOf(t) !== -1).length
-		);
+			const posts = $(".thumbnail-preview a").filter(
+				(_, el) =>
+					(parseInt(
+						($("img", el)
+							.attr("title")
+							.match(/score:(\d)+ /) || [])[1],
+						10
+					) || -1) >= 0 &&
+					!blacklist.filter((t) => $("img", el).attr("title").split(" ").indexOf(t) !== -1).length
+			);
 
-		if (posts.length <= 0) return null;
+			if (posts.length <= 0) return null;
 
-		const postURL = "https:" + posts.eq(Math.round(Math.random() * (posts.length - 1))).attr("href");
+			const postURL = "https:" + posts.eq(Math.round(Math.random() * (posts.length - 1))).attr("href");
 
-		const $p = await requestHTML(postURL);
+			const $p = await requestHTML(postURL);
 
-		return [
-			$p('[id^="psc"]').text(),
-			$p('[id^="psc"]').parent().prev().text().substr(8),
-			$p("meta[property='og:image']").attr("content"),
-		];
+			const imageURL = $p("meta[property='og:image']").attr("content");
+			const cleanPostURL = $p("link[rel='canonical']").attr("href");
+
+			return {
+				postID: cleanPostURL.substring(cleanPostURL.lastIndexOf("=") + 1),
+				postURL: cleanPostURL,
+				imageURL: imageURL,
+				score: $p('[id^="psc"]').text(),
+				rating: $p('[id^="psc"]').parent().prev().text().substr(8).toLowerCase(),
+				fileExt: imageURL.substring(imageURL.lastIndexOf(".") + 1),
+				timestamp: $p('[id^="psc"]').parent().prevUntil("h3", "li").last().next().text().substring(8, 27),
+				characters: $p(".tag-type-character a:nth-child(2)")
+					.map((_, e) => $p(e).text().replace("_", " "))
+					.get(),
+			};
+		},
 	},
 
-	danbooru: async (blacklist, tags) => {
-		let url = `https://danbooru.donmai.us/posts.json?limit=30&tags=order:random%20${encodeURIComponent(
-			tags || ""
-		).trim()}`;
+	danbooru: {
+		name: "Danbooru",
+		icon: "https://github.com/Bionus/imgbrd-grabber/raw/gh-pages/assets/img/sources/danbooru.png",
+		look: async (blacklist, tags) => {
+			let url = `https://danbooru.donmai.us/posts.json?limit=30&tags=order:random%20${encodeURIComponent(
+				tags || ""
+			).trim()}`;
 
-		const json = await requestJSON(url);
+			const json = await requestJSON(url);
 
-		const posts = json.filter(
-			(p) => p.score >= 0 && !blacklist.filter((t) => p.tag_string.split(" ").indexOf(t) !== -1).length
-		);
+			const posts = json.filter(
+				(p) => p.score >= 0 && !blacklist.filter((t) => p.tag_string.split(" ").indexOf(t) !== -1).length
+			);
 
-		if (!posts.length) return null;
+			if (!posts.length) return null;
 
-		const post = posts[Math.round(Math.random() * (posts.length - 1))];
+			const post = posts[Math.round(Math.random() * (posts.length - 1))];
 
-		return [
-			post.score,
-			{ s: "Safe", q: "Questionable", e: "Explicit" }[post.rating],
-			post.file_ext === "zip" ? post.large_file_url : post.file_url,
-		];
+			return {
+				postID: post.id,
+				postURL: "https://danbooru.donmai.us/posts/" + post.id,
+				imageURL: post.file_ext === "zip" ? post.large_file_url : post.file_url,
+				score: post.score,
+				rating: { s: "safe", q: "questionable", e: "explicit" }[post.rating],
+				fileExt: post.file_ext,
+				timestamp: post.created_at,
+				characters: (post.tag_string_character || "").split(" ").map((c) => c.replace("_", " ")),
+			};
+		},
 	},
 };
 
@@ -120,16 +144,38 @@ module.exports = class LookupCommand extends Commando.Command {
 			.sort((a, b) => a.sort - b.sort)
 			.map((a) => a.value);
 
-		let result;
+		let post;
+		let booru;
 
 		for (const site of sitesToTry) {
-			result = await SITES[site](BLACKLIST, tags + " " + WHITELIST.join(" "));
+			booru = SITES[site];
+			post = await booru.look(BLACKLIST, tags + " " + WHITELIST.join(" "));
 
-			if (result) break;
+			if (post) break;
 		}
 
-		if (!result) return msg.reply(`couldn't find anything...`);
+		if (!post) return msg.reply(`couldn't find anything...`);
 
-		msg.reply(`Score: **${result[0]}**; Rating: **${result[1]}**\n${result[2]}`);
+		let characters = (post.characters.length
+			? post.characters.length > 4
+				? [...post.characters.slice(0, 4), post.characters.length - 4 + "+"]
+				: post.characters
+			: []
+		).join(", ");
+
+		let embed = new Discord.MessageEmbed({
+			title: "Post #" + post.postID + (characters.length ? ": " + characters : ""),
+			description: "**Score**: " + post.score + "; **Rating**: " + post.rating,
+			url: post.postURL,
+			timestamp: post.timestamp,
+			footer: { text: booru.name, icon_url: booru.icon },
+			image: {
+				url: post.imageURL,
+			},
+		});
+
+		await msg
+			.reply({ embed })
+			.then(() => (["webm", "mp4"].includes(post.fileExt) ? msg.channel.send(post.imageURL) : null));
 	}
 };
