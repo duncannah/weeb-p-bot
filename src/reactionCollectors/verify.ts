0ec49756d98c01c8744701b8f9a7e6b69a71c768
Duncan <38371914+duncannah@users.noreply.github.com>
2020-05-06 13:52:32 +0200

    Don't throw error when bot reacts self

--- src/reactionCollectors/verify.ts
+++ src/reactionCollectors/verify.ts
@@ -5,11 +5,15 @@ export default function (message: Discord.Message): void {
 	const collector = message.createReactionCollector((reaction) => reaction.emoji.name === "✅");
 
 	collector.on("collect", (_, user) => {
-		message instanceof Commando.CommandoMessage && message.client!.user!.id !== user.id
-			? message
-					.guild!.members.fetch(user)
-					.then((member) => member.roles.add(message.guild.settings.get("verificatorMessage").verifiedRole))
-					.catch((err) => console.error(err))
+		message instanceof Commando.CommandoMessage
+			? message.client!.user!.id !== user.id
+				? message
+						.guild!.members.fetch(user)
+						.then((member) =>
+							member.roles.add(message.guild.settings.get("verificatorMessage").verifiedRole)
+						)
+						.catch((err) => console.error(err))
+				: null
 			: console.error(new Error(), message);
 	});
 }
